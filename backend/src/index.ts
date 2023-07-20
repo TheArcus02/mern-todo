ed4d442b9746512bc3f44dc304a8f2e084a3e5b2
TheArcus02 <m.urbaniak.inf@gmail.com>
2023-07-20 18:46:17 +0200

    connection to db and todo model

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,19 +1,32 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import postsRoutes from './routes/posts.routes';
+import todosRoutes from './routes/todos.routes';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+// Middlewares
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-app.use('/api/posts', postsRoutes);
+// Routes
+app.use('/api/todos', todosRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}`);
-});
+// Database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to database');
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
