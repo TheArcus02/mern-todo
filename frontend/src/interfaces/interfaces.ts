ae5fe58cd56f6a343d3d896aee97e5790fe67571
TheArcus02 <m.urbaniak.inf@gmail.com>
2023-07-24 16:46:34 +0200

    added user slice, new dependencies

--- frontend/src/interfaces/interfaces.ts
+++ frontend/src/interfaces/interfaces.ts
@@ -1,9 +1,14 @@
 export interface TodoInterface {
-  _id: string;
-  title: string;
-  description: string;
-  completed: boolean;
-  createdAt: string;
-  updatedAt: string;
-  __v: number;
+  _id: string
+  title: string
+  description: string
+  completed: boolean
+  createdAt: string
+  updatedAt: string
+  __v: number
+}
+
+export interface userInterface {
+  email: string
+  token: string
 }
